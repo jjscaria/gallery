0815f95c9285d22fec45a7244f78ccdb11d151c4
Jithin Scaria <github@jjscaria.com>
2024-02-08 15:36:43 -0600

    error handling

--- thumbnail_resize.sh
+++ thumbnail_resize.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-
-cp images/* images/fulls
-cp images/fulls/* images/thumbs
-echo "images transferred"
-rm images/*.jpg
-rm images/*.JPG
-rm images/*.png
+echo "Transferring images..."
+cp images/* images/fulls 2>/dev/null
+cp images/fulls/* images/thumbs 2>/dev/null
+echo "Images transferred"
+rm images/*.jpg 2>/dev/null
+rm images/*.JPG 2>/dev/null
+rm images/*.png 2>/dev/null
+echo "Generating thumbnails..."
 for f in images/thumbs/*; do convert "$f" -resize 510 "$f"; done 
-echo "thumbs generated"
-
+echo "Thumbnails generated"
